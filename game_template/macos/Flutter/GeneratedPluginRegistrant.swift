f9bd344ad9864b3db1b90e821f4aa70fc00b5263
Rich Hall <rjhall@gmail.com>
2022-12-16 16:05:59 +0800

    Firebase enabled. Ready to add Remote Config.

--- game_template/macos/Flutter/GeneratedPluginRegistrant.swift
+++ game_template/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,21 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import firebase_analytics
 import firebase_core
 import firebase_crashlytics
 import games_services
+import in_app_purchase_storekit
 import path_provider_macos
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   GamesServicesPlugin.register(with: registry.registrar(forPlugin: "GamesServicesPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
